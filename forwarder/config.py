5f4258e013b3a0bd06cb29fcb29b129af87b8e13
Coder-265 <93988223+Coder-265@users.noreply.github.com>
2022-07-24 13:33:32 +0530

    Update config.py

--- forwarder/config.py
+++ forwarder/config.py
@@ -1,11 +1,11 @@
 from forwarder.sample_config import Config
 
 class Development(Config):
-    API_KEY = "5294527571:AAFbjyBXwCxnP4qLg8CS0PB3DPseKYR532g"  # Your bot API key
+    API_KEY = "5076883469:AAHBE3rqirV3AL8hiYvl6bok_0AW6pghsqg"  # Your bot API key
     OWNER_ID = 862271564  # Your user id
 
     # Make sure to include the '-' sign in group and channel ids.
-    FROM_CHATS = [-1001787156904, -1001440212716]
+    FROM_CHATS = [-1001542591369,-1001440212716]
     TO_CHATS = [-1001703238624]
    
     REMOVE_TAG = False
